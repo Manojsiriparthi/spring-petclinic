8afdb1f4f5922d54227b3c2cce0af1057a68e9d3
Manoj kumar Siriparthi <manojs.devops1@gmail.com>
2024-11-12 05:01:15 +0000

    Update PostgresIntegrationTests.java

--- src/test/java/org/springframework/samples/petclinic/PostgresIntegrationTests.java
+++ src/test/java/org/springframework/samples/petclinic/PostgresIntegrationTests.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2012-2019 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.springframework.samples.petclinic;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -48,8 +32,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.client.RestTemplate;
 import org.testcontainers.DockerClientFactory;
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, properties = { "spring.docker.compose.skip.in-tests=false", //
-		"spring.docker.compose.profiles.active=postgres" })
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, properties = { "spring.docker.compose.skip.in-tests=false", "spring.docker.compose.profiles.active=postgres" })
 @ActiveProfiles("postgres")
 @DisabledInNativeImage
 public class PostgresIntegrationTests {
@@ -68,16 +51,6 @@ public class PostgresIntegrationTests {
 		assumeTrue(DockerClientFactory.instance().isDockerAvailable(), "Docker not available");
 	}
 
-	public static void main(String[] args) {
-		new SpringApplicationBuilder(PetClinicApplication.class) //
-			.profiles("postgres") //
-			.properties( //
-					"spring.docker.compose.profiles.active=postgres" //
-			) //
-			.listeners(new PropertiesLogger()) //
-			.run(args);
-	}
-
 	@Test
 	void testFindAll() throws Exception {
 		vets.findAll();
@@ -90,61 +63,4 @@ public class PostgresIntegrationTests {
 		ResponseEntity<String> result = template.exchange(RequestEntity.get("/owners/1").build(), String.class);
 		assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
-
-	static class PropertiesLogger implements ApplicationListener<ApplicationPreparedEvent> {
-
-		private static final Log log = LogFactory.getLog(PropertiesLogger.class);
-
-		private ConfigurableEnvironment environment;
-
-		private boolean isFirstRun = true;
-
-		@Override
-		public void onApplicationEvent(ApplicationPreparedEvent event) {
-			if (isFirstRun) {
-				environment = event.getApplicationContext().getEnvironment();
-				printProperties();
-			}
-			isFirstRun = false;
-		}
-
-		public void printProperties() {
-			for (EnumerablePropertySource<?> source : findPropertiesPropertySources()) {
-				log.info("PropertySource: " + source.getName());
-				String[] names = source.getPropertyNames();
-				Arrays.sort(names);
-				for (String name : names) {
-					String resolved = environment.getProperty(name);
-
-					assertNotNull(resolved, "resolved environment property: " + name + " is null.");
-
-					Object sourceProperty = source.getProperty(name);
-
-					assertNotNull(sourceProperty, "source property was expecting an object but is null.");
-
-					assertNotNull(sourceProperty.toString(), "source property toString() returned null.");
-
-					String value = sourceProperty.toString();
-					if (resolved.equals(value)) {
-						log.info(name + "=" + resolved);
-					}
-					else {
-						log.info(name + "=" + value + " OVERRIDDEN to " + resolved);
-					}
-				}
-			}
-		}
-
-		private List<EnumerablePropertySource<?>> findPropertiesPropertySources() {
-			List<EnumerablePropertySource<?>> sources = new LinkedList<>();
-			for (PropertySource<?> source : environment.getPropertySources()) {
-				if (source instanceof EnumerablePropertySource enumerable) {
-					sources.add(enumerable);
-				}
-			}
-			return sources;
-		}
-
-	}
-
 }
